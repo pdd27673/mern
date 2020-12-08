fac842933f84294d9345fe38a94fd70264b27d7e
Paul Doho <paul.doho.741@my.csun.edu>
2020-12-07 19:38:29 -0800

    Server

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post");
+
 router.post("/createpost", requireLogin, (req, res) => {
 	const { title, body } = req.body;
 	if (!title || !body) {
